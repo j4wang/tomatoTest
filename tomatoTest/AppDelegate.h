2d7f3dc3d279e6b490cbccf48ecc622501fbbf1f
JW <j4wang@gmail.com>
2013-10-18 21:12:27 -0700

    Initial Commit

---
+++ tomatoTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tomatoTest
+//
+//  Created by John on 10/18/13.
+//  Copyright (c) 2013 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tomatoTest/BoxOfficeViewController.m
@@ -0,0 +1,38 @@
+//
+//  BoxOfficeViewController.m
+//  tomatoTest
+//
+//  Created by John on 10/18/13.
+//  Copyright (c) 2013 John. All rights reserved.
+//
+
+#import "BoxOfficeViewController.h"
+
+@interface BoxOfficeViewController ()
+
+@end
+
+@implementation BoxOfficeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
