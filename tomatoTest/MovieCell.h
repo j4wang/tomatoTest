dd57f5f54f5c183c8c6bb54cbf15dff1545d24bb
JW <j4wang@gmail.com>
2013-10-18 21:34:46 -0700

    Update

---
+++ tomatoTest/MovieCell.h
@@ -0,0 +1,13 @@
+//
+//  MovieCell.h
+//  tomatoTest
+//
+//  Created by John on 10/18/13.
+//  Copyright (c) 2013 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieCell : UITableViewCell
+
+@end
---
+++ tomatoTest/MovieCell.m
@@ -0,0 +1,36 @@
+//
+//  MovieCell.m
+//  tomatoTest
+//
+//  Created by John on 10/18/13.
+//  Copyright (c) 2013 John. All rights reserved.
+//
+
+#import "MovieCell.h"
+
+@interface MovieCell ()
+
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+@property (nonatomic, weak) IBOutlet UILabel *starsLabel;
+
+@end
+
+@implementation MovieCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
